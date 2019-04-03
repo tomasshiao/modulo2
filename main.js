5a02ca06f3d61c5c77e02bf87185dd3f2d8fbad4
tomasshiao <tomasshiao2001@gmail.com>
2019-04-03 10:56:24 -0300

    terminado hasta ejercicio 5

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+var myName = "Tomás";
+var age = 18;
+var ignasiAge = 32;
+var ageDiff = age - ignasiAge;
+
 function a() {
   console.log("Starting JavaScript...");
+  console.log(myName);
+  console.log(age);
+  console.log(ageDiff);
 }
-var myName = "Tomás";
-console.log(myName);
+if(age<21){
+  console.log("You're not older than 21");
+} else if(age>21) {
+  console.log("You're older than 21");
+} else{
+  console.log("You're 21");
+}
+
+if (ageDiff>0){
+  console.log("Ignasi is younger than you");
+} else if (ageDiff<0){
+  console.log("Ignasi is older than you");
+} else {
+  console.log("You have the same age as Ignasi");
+}
