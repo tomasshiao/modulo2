4526b64ae80e99fd32cf1a9575299f260eb19246
tomasshiao <tomasshiao2001@gmail.com>
2019-04-22 11:14:55 -0300

    don't stop me now, cristinet

--- Script/main.js
+++ Script/main.js
@@ -1,31 +1,26 @@
-var senators = data.results[0].members.map(function (u){
-if(u.middle_name === null){
-  return u.last_name + ", " + u.first_name;
-} else {
-  return u.last_name + ",  " + u.first_name + " " + u.middle_name;
-}
-});
-//Nombres en diferentes tr
-var senadores = senators;
-for(var i=0; i<senators.length;i++){
-  let senador = document.createTextNode(senadores[i]);
-  document.getElementById("senate-data").insertRow().insertCell().appendChild(senador);
-}
-//Partidos de los senadores
-var party = data.results[0].members.map(function (x){
-  if(x.party === "R"){
-    return "R";
-  } else if (x.party === "D"){
-    return "D";
+var hallo = JSON.stringify(data,null,2);
+var tableHead = document.createElement("thead");
+var tableBody = document.createElement("tbody");
+document.getElementById("senate-data").innerHTML = tableHead += '<tr><th>Full Name</th><th>Party</th><th>State</th><th>Seniority</th><th>Percentage of votes with Party</th></tr>';
+var fullName;
+var senators = data.results[0].members.map(function(u) {
+  var tableRow = tableBody.insertRow();
+  if (u.middle_name === null) {
+    fullName = u.last_name + ", " + u.first_name;
   } else {
-    return "I";
+    fullName = u.last_name + ",  " + u.first_name + " " + u.middle_name;
   }
+  var cell = tableRow.insertCell();
+  function newCells(string) {
+    cell.appendChild(document.createTextNode(string));
+  }
+  newCells(fullName + " ");
+  newCells(u.party + " ");
+  newCells(u.state + " ");
+  newCells(u.seniority + " ");
+  newCells(u.votes_with_party_pct + "%");
 });
-var tabla = document.getElementById("senate-data");
-    // for (i = 0; i < tabla.rows.length; i++) {
-    //     createCell(tabla.rows[i].insertCell(tbl.rows[i].cells.length));
-        for(var u=0; u<party.length; u++){
-        let partido = document.createTextNode(party[u]);
-        document.getElementById("senate-data").HTMLTableRowElement().insertCell(0).appendChild(partido);
-      }
-    // }
+//
+//
+// document.getElementById("senate-data").appendChild(tableHead);
+document.getElementById("senate-data").appendChild(tableBody);
