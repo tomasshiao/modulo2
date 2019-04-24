c36c8bd56606b704fca3b8c972b41f25bbcdef95
tomasshiao <tomasshiao2001@gmail.com>
2019-04-24 10:57:40 -0300

    + Bootstrap CDN

--- Script/main.js
+++ Script/main.js
@@ -3,7 +3,7 @@ var tableHead = document.createElement("thead");
 var tableBody = document.createElement("tbody");
 document.getElementById("senate-data").innerHTML = tableHead += '<tr><th>Full Name</th><th>Party</th><th>State</th><th>Seniority</th><th>Percentage of votes with Party</th></tr>';
 var fullName;
-var senators = data.results[0].members.map(function(u) {
+var senators = data.results[0].members.forEach(function(u) {
   var tableRow = tableBody.insertRow();
   if (u.middle_name === null) {
     fullName = u.last_name + ", " + u.first_name;
@@ -14,9 +14,9 @@ var senators = data.results[0].members.map(function(u) {
   fullName = '<a href="'+ u.url +'">' + fullName + '</a>';
   var celda = tableRow.insertCell(0);
   celda.innerHTML = fullName;
-  tableRow.insertCell(1).appendChild(document.createTextNode(u.party + " "));
-  tableRow.insertCell(2).appendChild(document.createTextNode(u.state + " "));
-  tableRow.insertCell(3).appendChild(document.createTextNode(u.seniority + " "));
+  tableRow.insertCell(1).appendChild(document.createTextNode(u.party));
+  tableRow.insertCell(2).appendChild(document.createTextNode(u.state));
+  tableRow.insertCell(3).appendChild(document.createTextNode(u.seniority));
   tableRow.insertCell(4).appendChild(document.createTextNode(u.votes_with_party_pct + "%"));
 });
 
