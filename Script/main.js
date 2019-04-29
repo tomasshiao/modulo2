68befe9d950cb81983e32d07541744cf5fdffb7f
tomasshiao <tomasshiao2001@gmail.com>
2019-04-29 12:39:28 -0300

    *-*

--- Script/main.js
+++ Script/main.js
@@ -1,10 +1,11 @@
 var hallo = JSON.stringify(data,null,2);
 var tableHead = document.createElement("thead");
 var tableBody = document.createElement("tbody");
-
-document.getElementById("tablas").innerHTML = tableHead += '<tr><th>Full Name</th><th>Party</th><th>State</th><th>Seniority</th><th>Percentage of votes with Party</th></tr>';
+var members = data.results[0].members;
+function crearTabla(members){
+document.getElementById("tablas").innerHTML = (tableHead += '<tr><th>Full Name</th><th>Party</th><th>State</th><th>Seniority</th><th>Percentage of votes with Party</th></tr>');
 var fullName;
-var senators = data.results[0].members.forEach(function(u) {
+var senators = members.forEach(function(u) {
   var tableRow = tableBody.insertRow();
   if (u.middle_name === null) {
     fullName = u.last_name + ", " + u.first_name;
@@ -22,3 +23,22 @@ var senators = data.results[0].members.forEach(function(u) {
 });
 
 document.getElementById("tablas").appendChild(tableBody);
+}
+crearTabla(members);
+function filterMembers(members){
+      var checkBoxes = document.querySelectorAll('input[name=party-filter]:checked')
+      console.log(checkBoxes);
+      checkedBoxes = Array.from(checkBoxes)
+      console.log(checkedBoxes);
+      checkedBoxes = checkedBoxes.map(function (element) {
+          return element.value
+      })
+      console.log(checkedBoxes);
+      var filtrados = [];
+      filtrados = members.filter(function (members) {
+          if (checkedBoxes.includes(members.party)) {
+              return members;
+          }
+      })
+      return filtrados;
+  }
