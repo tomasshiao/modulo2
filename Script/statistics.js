25026c82e1ffb4d9a57702c023ce49cadea8d9af
tomasshiao <tomasshiao2001@gmail.com>
2019-05-15 11:10:53 -0300

    listo objeto

--- Script/statistics.js
+++ Script/statistics.js
@@ -8,6 +8,7 @@ var statistics =
   "democrats_avg_vwp": 0,
   "republicans_avg_vwp": 0,
   "independent_avg_vwp": 0,
+  "total_avg": 0,
   "least_engaged": [],
   "most_engaged": [],
   "least_loyal": [],
@@ -21,18 +22,28 @@ function crearListas(members){
     for(var i = 0; i<members.length; i++){
       if (members[i].party === "D"){
         dem.push(members[i]);
+        statistics.number_of_democrats = dem.length;
+        statistics.democrats_avg_vwp += members[i].votes_with_party_pct;
       }else if(members[i].party === "R"){
         rep.push(members[i]);
+        statistics.number_of_republicans = rep.length;
+        statistics.republicans_avg_vwp += members[i].votes_with_party_pct;
       } else if(members[i].party === "I") {
         ind.push(members[i]);
+        statistics.number_of_independent = ind.length;
+        statistics.independent_avg_vwp += members[i].votes_with_party_pct;
       }
     }
-    statistics.number_of_democrats = dem.length;
-    statistics.number_of_republicans = rep.length;
-    statistics.number_of_independent = ind.length;
+    statistics.democrats_avg_vwp = Number(parseFloat(statistics.democrats_avg_vwp/dem.length).toPrecision(4));
+    statistics.republicans_avg_vwp = Number(parseFloat(statistics.republicans_avg_vwp/rep.length).toPrecision(4));
+    statistics.independent_avg_vwp = Number(parseFloat(statistics.independent_avg_vwp/ind.length).toPrecision(4));
+    statistics.total_avg = Number(parseFloat((statistics.democrats_avg_vwp + statistics.republicans_avg_vwp + statistics.independent_avg_vwp)/3).toPrecision(5));
 }
 crearListas(members);
 
-function valoresPromedio(){
-  dem.map(m=>m.votes_with_party_pct).reduce(num1+num2,0);
+var tableBody = document.createElement("tbody");
+tableBody.setAttribute("id", "at-a-glance");
+function crearTablaAtAGlance(statistics){
+
 }
+crearTablaAtAGlance(statistics);
