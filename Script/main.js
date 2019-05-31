6f546593a311b3352eb8cc26a276ef0c768c2ade
Tomás Shiao <tomasshiao2001@gmail.com>
2019-05-31 06:32:36 -0300

    reiniciando

--- Script/main.js
+++ Script/main.js
@@ -3,35 +3,35 @@
 //   crearTabla(members);
 //   return members;
 // }
-var tableBody = document.createElement("tbody");
-tableBody.setAttribute('id', 'tabla');
-function crearTabla(members) {
-  members = filterMembers(members);
-  var fullName;
-  var senators = members.forEach(function(u) {
-    var tableRow = tableBody.insertRow();
-    tableRow.className = "rowBody";
-    if (u.middle_name === null) {
-      fullName = u.last_name + ", " + u.first_name;
-    } else {
-      fullName = u.last_name + ",  " + u.first_name + " " + u.middle_name;
-    }
-
-    var full_name = '<a href="' + u.url + '">' + fullName + '</a>';
-    u = {
-      ...u,
-      "full_name": fullName
-    };
-    var celda = tableRow.insertCell(0);
-    celda.innerHTML = full_name;
-    tableRow.insertCell(1).appendChild(document.createTextNode(u.party));
-    tableRow.insertCell(2).appendChild(document.createTextNode(u.state));
-    tableRow.insertCell(3).appendChild(document.createTextNode(u.seniority));
-    tableRow.insertCell(4).appendChild(document.createTextNode(u.votes_with_party_pct + "%"));
-  });
-
-  document.getElementById("tablas").appendChild(tableBody);
-}
+// var tableBody = document.createElement("tbody");
+// tableBody.setAttribute('id', 'tabla');
+// function crearTabla(members) {
+//   members = filterMembers(members);
+//   var fullName;
+//   var senators = members.forEach(function(u) {
+//     var tableRow = tableBody.insertRow();
+//     tableRow.className = "rowBody";
+//     if (u.middle_name === null) {
+//       fullName = u.last_name + ", " + u.first_name;
+//     } else {
+//       fullName = u.last_name + ",  " + u.first_name + " " + u.middle_name;
+//     }
+//
+//     var full_name = '<a href="' + u.url + '">' + fullName + '</a>';
+//     u = {
+//       ...u,
+//       "full_name": fullName
+//     };
+//     var celda = tableRow.insertCell(0);
+//     celda.innerHTML = full_name;
+//     tableRow.insertCell(1).appendChild(document.createTextNode(u.party));
+//     tableRow.insertCell(2).appendChild(document.createTextNode(u.state));
+//     tableRow.insertCell(3).appendChild(document.createTextNode(u.seniority));
+//     tableRow.insertCell(4).appendChild(document.createTextNode(u.votes_with_party_pct + "%"));
+//   });
+//
+//   document.getElementById("tablas").appendChild(tableBody);
+// }
 
 function filterMembers(members) {
   var checkBoxes = document.querySelectorAll('input[name=party-filter]:checked')
@@ -77,8 +77,22 @@ function usingVue(members){
 
 
 // -------------
-
+function createTables(members){
+  var fullName;
+  var senators = members.forEach(function(u) {
+    if (u.middle_name === null) {
+      fullName = u.last_name + ", " + u.first_name;
+    } else {
+      fullName = u.last_name + ",  " + u.first_name + " " + u.middle_name;
+    u = {
+      ...u,
+      "full_name": fullName
+    };
+    }
+    console.log(fullName);
+  })
 var app = new Vue({
   el:"#tables",
   data: {data:members}
 });
+}
--- Script/pro-congress-113-senate.js
+++ Script/pro-congress-113-senate.js
@@ -8,7 +8,33 @@ fetch('https://api.propublica.org/congress/v1/113/senate/members.json',{
     return response.json();
   })
   .then(function(myJson) {
-    data = myJson;
-    members = data.results[0].members;
-    initialise();
+    data = myJson,
+    // JSON.stringify(data, null, 2));
+    // initialise();
+    members = data.results[0].members
+    //console.log(members)
+  })
+  .then(createTables(members),
+  filterMembers(members))
+  .catch(function(error){
+     console.log("error")
+  })
+  function createTables(members){
+    var fullName;
+    var senators = members.forEach(function(u) {
+      if (u.middle_name === null) {
+        fullName = u.last_name + ", " + u.first_name;
+      } else {
+        fullName = u.last_name + ",  " + u.first_name + " " + u.middle_name;
+      u = {
+        ...u,
+        "full_name": fullName
+      };
+      }
+      console.log(fullName);
+    })
+  var app = new Vue({
+    el:"#tables",
+    data: {data:members}
   });
+  }
